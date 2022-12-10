f2085e8d40d265c76d9609f61bd942fe46a0e265
Jackietruong2805 <truonghao2805@gmail.com>
2022-12-10 08:58:11 +0700

    HtmlParser.10.12.2022

---
+++ HtmlParser/HtmlParser/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblPlainText = new System.Windows.Forms.Label();
+            this.txtHtmlRaw = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(251, 44);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "HTML Parser";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(441, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 83);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblPlainText
+            // 
+            this.lblPlainText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lblPlainText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlainText.ForeColor = System.Drawing.Color.Black;
+            this.lblPlainText.Location = new System.Drawing.Point(561, 189);
+            this.lblPlainText.Name = "lblPlainText";
+            this.lblPlainText.Padding = new System.Windows.Forms.Padding(10);
+            this.lblPlainText.Size = new System.Drawing.Size(409, 435);
+            this.lblPlainText.TabIndex = 5;
+            this.lblPlainText.Click += new System.EventHandler(this.lblPlainText_Click);
+            // 
+            // txtHtmlRaw
+            // 
+            this.txtHtmlRaw.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.txtHtmlRaw.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHtmlRaw.ForeColor = System.Drawing.Color.Black;
+            this.txtHtmlRaw.Location = new System.Drawing.Point(12, 186);
+            this.txtHtmlRaw.Multiline = true;
+            this.txtHtmlRaw.Name = "txtHtmlRaw";
+            this.txtHtmlRaw.Size = new System.Drawing.Size(409, 438);
+            this.txtHtmlRaw.TabIndex = 6;
+            this.txtHtmlRaw.TextChanged += new System.EventHandler(this.txtHtmlRaw_TextChanged);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(441, 299);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(94, 83);
+            this.btnReset.TabIndex = 7;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(441, 541);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(94, 83);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(261, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 60);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "HTML Raw";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(559, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 60);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Plain Text";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp4.Properties.Resources._73_736787_straight_arrow_svg_png_icon_free_download_straight;
+            this.pictureBox1.Location = new System.Drawing.Point(416, 110);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(146, 60);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(982, 636);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txtHtmlRaw);
+            this.Controls.Add(this.lblPlainText);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "HtmlParser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblPlainText;
+        private System.Windows.Forms.TextBox txtHtmlRaw;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ HtmlParser/HtmlParser/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using HtmlAgilityPack;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+
+
+namespace WindowsFormsApp4
+{
+   
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+        public void button1_Click(object sender, EventArgs e)
+        {
+            string html = txtHtmlRaw.Text;
+            HtmlDocument htmldoc = new HtmlDocument();
+            htmldoc.LoadHtml(html);
+            QueuesLinked q = new QueuesLinked();
+            var body = htmldoc.DocumentNode.SelectSingleNode("//body");
+            HtmlNodeCollection childnodes = body.ChildNodes;
+            lblPlainText.Text = "";
+            if (q.checkHtmlTag(html))
+            {
+                if (htmldoc.ParseErrors.Count() > 0)
+                {
+                    lblPlainText.Text = "Syntax error";
+                }
+                else
+                {
+                    foreach (var node in childnodes)
+                    {
+                        if (node.NodeType == HtmlNodeType.Element)
+                        {
+                            q.enqueue(node.InnerText);
+                        }
+                    }
+                }
+                string[] textList = q.display();
+                foreach (string text in textList)
+                {
+                    lblPlainText.Text += "--> " + text + @"
+";
+                }
+            }
+            else
+            {
+                lblPlainText.Text = "Syntax error";
+            }
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtHtmlRaw.Text = "";
+            lblPlainText.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void lblPlainText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtHtmlRaw_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
